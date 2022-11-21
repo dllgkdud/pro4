170ba730de9b50425f7be36ce6d27280d75be700
dllgkdud <dllgkdud@gmail.com>
2022-11-21 18:48:10 +0900

    Spring legacy start

---
+++ pro04/src/main/java/com/samsung/dto/MemberDTO.java
@@ -0,0 +1,61 @@
+package com.samsung.dto;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String name;
+	private String tel;
+	private String addr;
+	private String regdate;
+	private int pt;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public int getPt() {
+		return pt;
+	}
+	public void setPt(int pt) {
+		this.pt = pt;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", pw=" + pw + ", name=" + name + ", tel=" + tel + ", addr=" + addr
+				+ ", regdate=" + regdate + ", pt=" + pt + "]";
+	}
+}
